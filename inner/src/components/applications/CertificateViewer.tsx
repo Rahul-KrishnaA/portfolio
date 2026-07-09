822471622ee7bc352a4ffd1a0239a2cb3885f651
Rahul-KrishnaA <rahulkrishna1662004@gmail.com>
2026-07-09 22:31:54 +0530

    Fix pdf.js worker path for downgraded pdfjs-dist v3

--- inner/src/components/applications/CertificateViewer.tsx
+++ inner/src/components/applications/CertificateViewer.tsx
@@ -3,7 +3,7 @@ import { Document, Page, pdfjs } from 'react-pdf';
 import Window from '../os/Window';
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-    'pdfjs-dist/build/pdf.worker.min.mjs',
+    'pdfjs-dist/build/pdf.worker.min.js',
     import.meta.url
 ).toString();
 
